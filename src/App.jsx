27d4b96aba1a5743fa9af9117ab7193843bb935d
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-12-23 02:52:59 +0200

    questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { QUESTIONS } from "../src/QUESTION_BANKS.js";
 import Header from "./components/Header.jsx";
-import { useState, useCallback } from "react";
+import { useState, useEffect } from "react";
 import Quiz from "./components/Quiz.jsx";
 import Result from "./components/Result.jsx";
 
 function App() {
   const [questionIndex, setQuestionIndex] = useState(0);
   const [isFinshed, setIsFinished] = useState(false);
-  // const currentQuestion = Question[questionIndex];
+  useEffect(() => {
+    if (questionIndex >= QUESTIONS.length) {
+      setIsFinished(true);
+    }
+  }, [questionIndex]);
 
   const NextQuestion = () => {
-    const nextIndex = questionIndex + 1;
-
-    if (nextIndex >= QUESTIONS.length) {
-      setIsFinished(true);
-    } else {
-      setQuestionIndex(nextIndex);
-    }
+    setQuestionIndex((prev) => prev + 1);
   };
 
   // const currentQuestion = QUESTIONS[questionIndex];
@@ -24,15 +22,12 @@ function App() {
   return (
     <>
       <Header />
-      {isFinshed ? (
-        <Result />
-      ) : (
-        <Quiz
-          Question={QUESTIONS}
-          questionIndex={questionIndex}
-          NextQuestion={NextQuestion}
-        />
-      )}
+
+      <Quiz
+        Question={QUESTIONS}
+        questionIndex={questionIndex}
+        NextQuestion={NextQuestion}
+      />
     </>
   );
 }
